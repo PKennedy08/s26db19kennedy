86421f953a56f15e59d208329c8acb80dbfc9fac
PKennedy08 <158314337+PKennedy08@users.noreply.github.com>
2026-04-08 20:36:04 -0500

    Update instruments.js

--- routes/instruments.js
+++ routes/instruments.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const mongoose = require("mongoose")
+const intrumentsSchema = mongoose.Schema({
+costume_type: String,
+size: String,
+cost: Number
+})
+module.exports = mongoose.model("Intruments",
+intrumentsSchemaSchema)
+
 router.get('/', function(req, res, next) {
   res.render('instruments', { title: 'Search Results Instruments' });
 });
