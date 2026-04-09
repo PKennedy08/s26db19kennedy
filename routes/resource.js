ceeff1b74dcdf4e0bbb1ba878baf1760207dc3c2
PKennedy08 <158314337+PKennedy08@users.noreply.github.com>
2026-04-08 23:17:08 -0500

    Update resource.js

---
+++ routes/resource.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+// Require controller modules.
+var api_controller = require('../controllers/api');
+var instrument_controller = require('../controllers/instrument');
+/// API ROUTE ///
+// GET resources base.
+router.get('/', api_controller.api);
+/// COSTUME ROUTES ///
+// POST request for creating a Costume.
+router.post('/instrument', instrument_controller.instrument_create_post);
+// DELETE request to delete Costume.
+router.delete('/instrument/:id', instrument_controller.instrument_delete);
+// PUT request to update Costume.
+router.put('/instrument/:id', instrument_controller.instrument_update_put);
+// GET request for one Costume.
+router.get('/instrument/:id', instrument_controller.instrument_detail);
+// GET request for list of all Costume items.
+router.get('/instrument', instrument_controller.instrument_list);
+module.exports = router;
