241be180715f862dcff64a98e43a54944fe4d091
PKennedy08 <158314337+PKennedy08@users.noreply.github.com>
2026-04-10 14:40:56 -0500

    Update resource.js

--- routes/resource.js
+++ routes/resource.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 // FIXED: correct controller import
 const instrument_controller = require('../controllers/instrument');
+var api_controller = require('../controllers/instrument')
+
+router.get('/', api_controller.api)
 
 // GET one
 router.get('/instruments/:id', instrument_controller.instrument_detail);
