1c84fff835ff27c681417f2dd84cdcd14ed7c7a6
PKennedy08 <158314337+PKennedy08@users.noreply.github.com>
2026-04-12 19:17:24 -0500

    Update instruments.js

--- routes/instruments.js
+++ routes/instruments.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 // IMPORT CONTROLLER
-const instrument_controller = require('../controllers/instrument');
+var instrument_controller = require('../controllers/instrument');
 
 
 router.get('/', instrument_controller.instrument_list);
