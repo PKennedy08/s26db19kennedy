252696f27a50e731dec85b768a5603718b66bffc
PKennedy08 <158314337+PKennedy08@users.noreply.github.com>
2026-04-23 18:11:28 -0500

    asdf

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,89 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
+var Account = require('../models/account');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// HOME PAGE
+router.get('/', function (req, res) {
+  res.render('index', { title: 'instrument App', user: req.user });
+});
+
+// REGISTER PAGE
+router.get('/register', function(req, res) {
+  res.render('register', { title: 'instrument App Registration' });
+});
+
+// REGISTER POST
+router.post('/register', function(req, res) {
+  Account.findOne({ username: req.body.username })
+    .then(function(user) {
+
+      if (user != null) {
+        console.log("exists " + req.body.username);
+        return res.render('register', {
+          title: 'Registration',
+          message: 'Existing User',
+          account: req.body.username
+        });
+      }
+
+      let newAccount = new Account({ username: req.body.username });
+
+      Account.register(newAccount, req.body.password, function(err, user) {
+        if (err) {
+          console.log("db creation issue " + err);
+          return res.render('register', {
+            title: 'Registration',
+            message: 'access error',
+            account: req.body.username
+          });
+        }
+
+        if (!user) {
+          return res.render('register', {
+            title: 'Registration',
+            message: 'access error',
+            account: req.body.username
+          });
+        }
+
+        console.log('Success, redirect');
+        res.redirect('/');
+      });
+    })
+    .catch(function(err) {
+      return res.render('register', {
+        title: 'Registration',
+        message: 'Registration error',
+        account: req.body.username
+      });
+    });
+});
+
+// LOGIN PAGE
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'instrument App Login', user: req.user });
+});
+
+// LOGIN POST
+router.post('/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+// LOGOUT
+router.get('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+// PING (test route)
+router.get('/ping', function(req, res) {
+  res.status(200).send("pong!");
 });
 
 module.exports = router;
